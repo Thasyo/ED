92a5e8c0274ff812e05cb43b0a54c0b56149ee12
Thasyo Peres Leite <thasyotjk@gmail.com>
2022-10-17 22:53:06 -0300

    atualizando progresso

--- js/script.js
+++ js/script.js
@@ -168,6 +168,44 @@ for(var i = 0; i < month.length; i++) {
     };
 };
 
+console.log("--- PILHAS ---");
+
+//"O último a entrar é o primeiro a sair - LIFO (Last In First Out)"
+
+function pilha() {
+
+    var items = [];
+
+    this.push = function() {
+        //adiciona um novo item a pilha.
+    };
+
+    this.pop = function() {
+        //remove o item do topo da pilha. 
+    };
+
+    this.peek = function() {
+        //devolve o elemento que está no topo da pilha.
+    };
+
+    this.isEmpty = function() {
+        //informa se a pilha está vazia ou não.
+    };
+
+    this.clear = function() {
+        //limpa a pilha.
+    };
+
+    this.size = function() {
+        //informa o tamanho da pilha.
+    };
+
+    this.print = function() {
+        //imprime a pilha no console.
+    };
+}
+
+
 
 
 
